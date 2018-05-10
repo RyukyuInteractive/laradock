47c2234a4435709079419bd506e9d47a389157c0
atomita <sleeping.cait.sith+gh@gmail.com>
2018-05-10 12:52:11 +0900

    fix: Rename theme

---
+++ _wordpress/themes/empty/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>empty</title>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <?php wp_footer(); ?>
+  </body>
+</html>
